ab3627fed56cd55c13837bbbc8137ea163ff405a
Bunyamin AKTAS <bunyaminaktas@outlook.com>
2020-05-03 21:08:33 +0300

    Answers were checked and corrected. Some explanations were edited.

--- HW3/src/Q47.java
+++ HW3/src/Q47.java
@@ -7,6 +7,8 @@ public class Q47 {
 		 */
 		// Original statement
 		//System.out.print((5 + (!2 + 8) * 3 - 3 % 2)/2);
+		
+		// If the error is fixed such as below, the output will be 17.
 		System.out.print((5 + (2 + 8) * 3 - 3 % 2)/2);
 	}
 }
