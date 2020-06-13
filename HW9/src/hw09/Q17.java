d060eba4e6bd97a096fb54ab20c1d931e8802e49
Bunyamin AKTAS <bunyaminaktas@outlook.com>
2020-06-13 21:12:38 +0300

    Question 17 was solved.

--- HW9/src/hw09/Q17.java
+++ HW9/src/hw09/Q17.java
@@ -2,7 +2,7 @@ package hw09;
 
 // Class name Shoot has been changed to Q17
 public class Q17 {
-	interface Target {
+	interface Target {	// a functional interface since it has only one method and that method returns a boolean value
 		boolean needToAim(double angle);
 	}
 	static void prepare(double angle, Target t) {
@@ -10,6 +10,7 @@ public class Q17 {
 		System.out.println(ready);
 	}
 	public static void main(String[] args) {
+		// lambda expression returns true if d is bigger than 5 or less than -5
 		prepare(45, d -> d > 5 || d < -5);	// k2
 	}
 
