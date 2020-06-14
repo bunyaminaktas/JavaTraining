0238f409b1f4aff0752fc850f453170064750900
Bunyamin AKTAS <bunyaminaktas@outlook.com>
2020-06-14 09:58:10 +0300

    Revamped the explanations in Q1, Q3, Q4, Q6, Q7, Q8, Q9, Q11, Q18, Q20, Q21, Q23. Corrected misinformation in Q2, option C. Written a working code in Q07.java

--- HW9/src/hw09/Q07.java
+++ HW9/src/hw09/Q07.java
@@ -3,10 +3,14 @@ package hw09;
 public class Q07 {
 
 	public static void main(String[] args) {
+		// Original code
 		/*
 		 * - Since sb is in the process of initialization, it cannot be used in expressions.
 		 */
 		//StringBuilder sb = new StringBuilder("radical").insert(sb.length(), "robots");
+		
+		StringBuilder sb = new StringBuilder("radical");
+		sb.insert(sb.length(), "robots");
+		System.out.println(sb);
 	}
-
 }
